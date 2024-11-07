60678875f5b867348247335abdcda5f6ec81e522
mapotso <mapotso04@gmail.com>
2024-11-07 20:07:19 +0200

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+
+const Home = () => {
+    return (
+        <div className="home">
+            <h1> KA LIJONG MASERU</h1>
+           
+           <img src="foot1.jpeg"  alt="ENJOYY" style={{ width: '100%', height: 'auto' }} />
+           <h1>TLOHO KA NAKO TSOHLE  O TLOJA</h1>
+            
+        </div>
+    );
+};
+
+export default Home;
